04646491368ae6e5d44fc8a5b1526d649ad468cf
rajoyraj <rajoyraj@gmail.com>
2021-07-04 22:57:33 +0600

    commit from asian time zone guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -1,21 +1,20 @@
 package qaclickacademy;
 
 import org.testng.annotations.Test;
+
 //rahulonlinetutor@gmail.com
 public class RESTAPITest {
 
 	@Test
-	public void postJira()
-	{
+	public void postJira() {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
 		System.out.println("postJira4");
 	}
-	
+
 	@Test
-	public void deleteTwitter()
-	{
-		System.out.println("deleteTwitter");	
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
 	}
 }
